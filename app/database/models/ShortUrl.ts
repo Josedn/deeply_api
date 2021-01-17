59af332311a096477ecbe9c0e600ac494a5e05dc
Jose D <joserafael161@hotmail.com>
2021-01-17 18:32:24 -0500

    Working on typescript mongoose schemas

--- app/database/models/ShortUrl.ts
+++ app/database/models/ShortUrl.ts
@@ -1,27 +1,15 @@
-// Import the neccesary modules.
-import mongoose from "mongoose";
+import mongoose, { Schema, Document } from "mongoose";
 
-// The schema used by mongoose.
-const ShortUrlSchema = new mongoose.Schema({
-    slug: String,
-    source: String,
+export interface IShortUrl extends Document {
+    slug: string;
+    source: string;
+}
+
+const ShortUrlSchema: Schema = new Schema({
+    slug: { type: String, required: true, unique: true },
+    source: { type: String, required: true },
 });
 
-/**
- * A model object for short urls.
- * @type {ShortUrl}
- */
-
-export type ShortUrlDao = {
-    slug: string,
-    source: string,
-};
-
-// Create the model.
 const ShortUrl = mongoose.model("ShortUrl", ShortUrlSchema);
 
-/**
- * A model object for short urls.
- * @type {ShortUrl}
- */
-export {ShortUrl};
+export default ShortUrl;
