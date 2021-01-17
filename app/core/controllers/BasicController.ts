537a090dd854225fd67bc74b1044e64dfe1bed4c
Jose D <joserafael161@hotmail.com>
2021-01-17 00:48:24 -0500

    Add logging

--- app/core/controllers/BasicController.ts
+++ app/core/controllers/BasicController.ts
@@ -2,10 +2,16 @@ import Logger, { LogLevel } from "../../misc/Logger";
 import { Request, Response, NextFunction } from "express";
 
 const writeLine = Logger.generateLogger("BasicController");
+const writeLineWithRequest = (line: String, req: Request) => {
+    const address = req.connection.address() as any;
+    if (address?.address != null) {
+        writeLine(line + " from " + address.address, LogLevel.Debug);
+    }
+};
 
 export default class BasicController {
     static getIndex(req: Request, res: Response, next: NextFunction): void {
-        writeLine("Requested index", LogLevel.Debug);
+        writeLineWithRequest("Requested index", req);
         res.json({});
     }
 }
